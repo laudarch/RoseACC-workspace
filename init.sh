53a630438c49d442c724ef6ba65a3c37aedf0f24
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2014-06-11 15:16:23 -0700

    Restored kernel generation after modification of libOpenACC (flexible tiling and internalization of kernels)

--- init.sh
+++ init.sh
@@ -2,32 +2,40 @@
 
 set -e
 
-TOP_WORKSPACE_DIR=`pwd`
+BOOST_HOME=/media/ssd/boost/install/1_45_0/
+OPENCL_HOME=/usr
+SQLITE_HOME=/media/ssd/lib/sqlite
 
-export LD_LIBRARY_PATH=/media/ssd/boost/install/1_45_0/lib:$LD_LIBRARY_PATH
+WORKSPACE_DIR=`pwd`
+BUILD_DIR=$WORKSPACE_DIR/workspace
+INSTALL_DIR=$BUILD_DIR/install
+
+MAKE_OPTIONS=-j8
+
+export LD_LIBRARY_PATH=$BOOST_HOME/lib:$LD_LIBRARY_PATH
 
 ./build
 
-mkdir -p workspace
+mkdir -p $BUILD_DIR
 
-pushd workspace
+cd $BUILD_DIR
 
-$TOP_WORKSPACE_DIR/configure --prefix=$TOP_WORKSPACE_DIR/workspace/install --with-boost=/media/ssd/boost/install/1_45_0 --with-opencl=/usr --with-sqlite=/media/ssd/lib/sqlite
+$WORKSPACE_DIR/configure --prefix=$INSTALL_DIR --with-boost=$BOOST_HOME --with-opencl=$OPENCL_HOME --with-sqlite=$SQLITE_HOME
 
-popd
+cd $WORKSPACE_DIR
 
-# Following 4 lines can be replaced by : make -C $TOP_WORKSPACE_DIR/workspace/rose/src install-core -j8 (it implies to build/install exampleTranslator and tutorials)
-make -C $TOP_WORKSPACE_DIR/workspace/rose/src -j8
-make -C $TOP_WORKSPACE_DIR/workspace/rose install-data-local
-make -C $TOP_WORKSPACE_DIR/workspace/rose/src install -j8
-cp $TOP_WORKSPACE_DIR/workspace/rose/rosePublicConfig.h $TOP_WORKSPACE_DIR/workspace/install/include
+# Following 4 lines can be replaced by : make -C $BUILD_DIR/rose/src install-core -j8 (it implies to build/install exampleTranslator and tutorials)
+make $MAKE_OPTIONS -C $BUILD_DIR/rose/src
+make $MAKE_OPTIONS -C $BUILD_DIR/rose install-data-local
+make $MAKE_OPTIONS -C $BUILD_DIR/rose/src install
+cp $BUILD_DIR/rose/rosePublicConfig.h $INSTALL_DIR/include
 
-make -C $TOP_WORKSPACE_DIR/workspace/RoseACC -j8
-make -C $TOP_WORKSPACE_DIR/workspace/RoseACC install
+make $MAKE_OPTIONS -C $BUILD_DIR/RoseACC
+make $MAKE_OPTIONS -C $BUILD_DIR/RoseACC install
 
-make -C $TOP_WORKSPACE_DIR/workspace/libOpenACC -j8
+make $MAKE_OPTIONS -C $BUILD_DIR/libOpenACC
 
-make -C $TOP_WORKSPACE_DIR/workspace/examples -j8
+make $MAKE_OPTIONS -C $BUILD_DIR/examples
 
-make -C $TOP_WORKSPACE_DIR/workspace/tests -j8
+make $MAKE_OPTIONS -C $BUILD_DIR/tests
 
